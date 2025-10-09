4624aa24f714fafed28f351909882989cadc115f
availablehybrid1 <availablehybrid1@gmail.com>
2025-10-09 15:39:23 -0700

    Delete pages/_home-temp.tsx

--- pages/_home-temp.tsx
+++
@@ -1,6 +0,0 @@
-// pages/index.tsx — test mínimo (para aislar el error)
-export default function Home() {
-  return <main style={{minHeight:"100vh",display:"grid",placeItems:"center",background:"#0a0a0a",color:"#fff"}}>
-    <h1>Home OK</h1>
-  </main>;
-}
