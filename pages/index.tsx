969dccf2c431b7c298de345068902f87503e1772
availablehybrid1 <availablehybrid1@gmail.com>
2025-10-09 15:43:35 -0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{minHeight:'100vh',display:'grid',placeItems:'center',background:'#0a0a0a',color:'#fff'}}>
+      <h1>Home OK</h1>
+    </main>
+  );
+}
