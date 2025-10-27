fc29333011be2fca36b16dd354003476b89cec38
availablehybrid1 <availablehybrid1@gmail.com>
2025-10-27 16:42:31 -0700

    Update financing.tsx

--- pages/financing.tsx
+++ pages/financing.tsx
@@ -2,19 +2,13 @@
 
 export async function getServerSideProps() {
   const dcUrl =
-    "https://dwssecuredforms.dealercenter.net/CreditApplication/index/28816065?themecolor=0d0d0d&formtype=l&standalone=true&ls=Other"; // SIN frameId
+    "https://dwssecuredforms.dealercenter.net/CreditApplication/index/288160657?themecolor=0d0d0d&formtype=l&standalone=true&ls=Other";
 
-  // Redirección HTTP 302; el navegador enviará Referer = /financing
   return {
-    redirect: {
-      destination: dcUrl,
-      permanent: false,
-    },
+    redirect: { destination: dcUrl, permanent: false },
   };
 }
 
-// Este componente nunca se renderiza porque el server redirige antes.
-// Lo dejamos por si Next requiere un default export.
 export default function FinancingRedirect() {
   return null;
 }
